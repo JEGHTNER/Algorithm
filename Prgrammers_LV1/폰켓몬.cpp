7dc4dd7d21f7b36a78ef40845f688ad7c38037e6
JEGHTNER <jeheon0717@naver.com>
2022-12-22 01:22:37 +0900

    <feat> : Programmers Lv1

---
+++ Prgrammers_LV1/폰켓몬.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int N = nums.size() / 2;
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    answer = min(int(nums.size()), N);
+    return answer;
+}
