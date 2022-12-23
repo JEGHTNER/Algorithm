2fe4eb9a473aa9c2e3c4a64b563486958534d8d3
JEGHTNER <jeheon0717@naver.com>
2022-12-23 23:41:21 +0900

    programmers lv1

--- Prgrammers_LV1/소수 찾기.cpp
+++ Prgrammers_LV1/소수 찾기.cpp
@@ -1,29 +1,35 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    vector<bool> lst={true,};
+    vector<bool> lst(n+1);
 
-    lst[0], lst[1] = false, false;
+    fill(lst.begin(),lst.end(),true);
+    lst[0] = false;
+    lst[1] = false;
     for (int i = 2; i * i <= n; i++)
     {
         if (lst[i])
         {
             for (int j = i*i; j <= n; j += i)
             {
-                lst[j] = false
+                lst[j] = false;
             }
         }
     }
+    for(int i = 0 ; i < lst.size(); i ++)
+        cout << lst[i];
+    answer = count(lst.begin(),lst.end(),true);
     return answer;
 }
 
-#include <iostream>
+
 
 int main()
 {
-    cout << solution(10)<<endl;
+    cout << solution(5)<<endl;
 }
