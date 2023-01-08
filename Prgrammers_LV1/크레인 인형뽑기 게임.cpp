e268ed71110b8be1c36e1953734e3befead75e80
JEGHTNER <jeheon0717@naver.com>
2023-01-08 16:45:29 +0900

    Programmers LV1

--- Prgrammers_LV1/크레인 인형뽑기 게임.cpp
+++ Prgrammers_LV1/크레인 인형뽑기 게임.cpp
@@ -1,10 +1,47 @@
 #include <string>
 #include <vector>
-
+#include <stack>
+#include "printVector.cpp"
 using namespace std;
+
+int check_stack(vector<int> *stack)
+{
+    int i = 0;
+    int count = 0;
+    for(int i = 0; i < (*stack).size() - 1; i++)
+    {
+        if ((*stack)[i] == (*stack)[i + 1])
+       {
+        cout<<"i="<<i<<endl;
+        count +=2;
+        (*stack).erase((*stack).begin() + (i + 1));
+        (*stack).erase((*stack).begin() + i);
+       }
+    }
+    return count;
+}
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
+    stack<int>  stac;
+    vector<int> test = {4,2, 3, 1, 1, 3, 2, 4};
+    for (int i = 0 ; i < moves.size(); i++)
+    {
+        for (int col = 0 ; col<board.size(); col++)
+        {
+            if (board[col][moves[i] - 1] != 0)
+            {
+                stack.push_back(board[col][moves[i] - 1]);
+                if (!stack.empty() && stack.top)
+                answer += check_stack(&stack);
+                check_stack(&test);
+                printVector(test);
+                board[col][moves[i] - 1] = 0;
+                break ;
+            }
+        }
+    }
+    //printVector(stack);
     return answer;
 }
 
