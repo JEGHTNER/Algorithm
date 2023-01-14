3f519a081b261ef3f74b4c4ad5486fb1f502158f
Jeheon Lee <jeheon0717@naver.com>
2023-01-14 19:20:50 +0900

    Programmers Lv1 -Python

--- Python/개인정보 수집 유효기간.py
+++ Python/개인정보 수집 유효기간.py
@@ -11,34 +11,41 @@ def check_date(date, num):
 	_month = int(date[5:7])
 	_day = int(date[8:10])
 	_month += num
-	if _month > 12:
+	while _month > 12:
 		_year += 1
 		_month -= 12
 	print(year,month,day)
 	print(_year,_month,_day)
-	if year < _year:
+	if year > _year:
 		return False
-	if year <= _year and month < _month:
-		return False
-	if year <= _year and month <= _month and day < _day :
-		return False
+	elif year == _year:
+		if month > _month:
+			return False
+		elif month == _month :
+			if day >= _day :
+				return False
 	return True
 	
 def solution(today, terms, privacies):
 	answer = []
+	ret = []
 	dic = {}
 	init_date(today)
 	for t in terms:
 		dic[t.split()[0]] = int(t.split()[1])
 	for date in privacies:
-		print(check_date(date.split()[0], dic[date.split()[1]]))
+		answer.append(check_date(date.split()[0], dic[date.split()[1]]))
+		print(answer)
+	for i in range(len(answer)):
+		if answer[i] == False:
+			ret.append(i+1)
 	print(dic)
-	return answer
+	return ret
 
 
 
-today = "2022.05.19"
-terms = ["A 6", "B 12", "C 3"]
-privacies=["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+today ="2022.05.19"	
+terms = ["A 6", "B 12", "C 3"]	
+privacies=["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]	
 
-solution(today, terms, privacies)
+print(solution(today, terms, privacies))
