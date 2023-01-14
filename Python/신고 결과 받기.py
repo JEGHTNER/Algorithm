83f6fe83efee7d95c76692847b780f1044457b40
Jeheon Lee <jeheon0717@naver.com>
2023-01-14 16:00:36 +0900

    <feat> : refactor Programmers Lv1-Python

--- Python/신고 결과 받기.py
+++ Python/신고 결과 받기.py
@@ -1,28 +1,16 @@
 def solution(id_list, report, k):
-    answer = []
+    answer = [0] * len(id_list)
     report = list(set(report))
-    dic ={}
-    reported=[]
-    for name in id_list:
-        dic[name] = [0,0]
-    print(dic)
-    for r in report:
-        tmp = r.split()
-        print(tmp)
-        dic[tmp[0]].append(tmp[1])
-        dic[tmp[1]][0] += 1
+    dic = {x : 0 for x in id_list}
 
-    for i in dic:
-        if dic[i][0] >= k:
-            reported.append(i)
-    for i in dic:
-        for rep in reported:
-            if rep in dic[i]:
-                dic[i][1] +=1
-    print(dic)
-    a = list(dic.values())
-    for i in a:
-        answer.append(i[1])
+    for r in report:
+        tmp = r.split()[1]
+        #print(tmp)
+        dic[tmp] += 1
+    #print(dic)
+    for r in report:
+        if dic[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
     return answer
     
 id_list = ["muzi", "frodo", "apeach", "neo"]
