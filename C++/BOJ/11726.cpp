c77c42a65c2ceb18f4a4c4bed1c90c528ae82d24
JEGHTNER <jeheon0717@naver.com>
2023-01-30 12:09:58 +0900

    BOJ 11726-c++

--- C++/BOJ/11726.cpp
+++ C++/BOJ/11726.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    cin.tie(0);
-    ios::sync_with_stdio(0);
+    // cin.tie(0);
+    // ios::sync_with_stdio(0);
     int dp[1001] = {0,};
     int n;
     
@@ -13,6 +13,6 @@ int main()
     dp[2] = 2;
     cin>>n;
     for (int i = 3; i <= n; i++)
-        dp[i] = dp[i - 2] + dp[i - 1];
+        dp[i] = dp[i - 2] %10007 + dp[i - 1] % 10007;
     cout<<dp[n] % 10007;
 }
