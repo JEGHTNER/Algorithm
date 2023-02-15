ab7fac187c070e095a4c2bb89bb83aca5aa168cc
JEGHTNER <jeheon0717@naver.com>
2023-02-15 21:50:53 +0900

    BOJ 11660 -c++

---
+++ C++/BOJ/11660.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n;
+    vector<vector<int>> dp(n,vector<int>(n, 0));
+    vector<vector<int>> table = {};
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> tmp = {};
+        for (int j = 0; j < n; j++)
+        {
+            int num;
+            cin>>num;
+            tmp.push_back(num);
+        }
+        table.push_back(tmp);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + table[i - 1][j - 1];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int x1, y1, x2, y2;
+        cin>>x1>>y1>>x2>>y2;
+    }
+}
