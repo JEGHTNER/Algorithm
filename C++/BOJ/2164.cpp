2829578c5827408a90afc93b9d384e1a1a5752d6
이제헌 <jeheon0717@naver.com>
2023-02-17 13:50:26 +0900

    BOJ 2164 -c++

--- C++/BOJ/2164.cpp
+++ C++/BOJ/2164.cpp
@@ -11,8 +11,15 @@ int main()
 
     deque<int> queue;
     for (int i = 1; i <= n; i++)
-    {
         queue.push_back(i);
+    while (queue.size())
+    {
+        if (queue.size() <= 1)
+            break;
+        queue.pop_front();
+        int tmp = queue.front();
+        queue.pop_front();
+        queue.push_back(tmp);
     }
-
+    cout<<queue.front();
 }
