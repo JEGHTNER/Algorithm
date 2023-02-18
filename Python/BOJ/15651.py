60d0ff3ccfb8dfde4553ae066892c1ec0a70c5f1
이제헌 <jeheon0717@naver.com>
2023-02-18 13:33:53 +0900

    BOJ 15651 -py

---
+++ Python/BOJ/15651.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+lst = []
+
+
+def dfs():
+    if len(lst) == m:
+        print(' '.join(map(str, lst)))
+        return
+    for i in range(1, n + 1):
+        lst.append(i)
+        dfs()
+        lst.pop()
+
+dfs()
