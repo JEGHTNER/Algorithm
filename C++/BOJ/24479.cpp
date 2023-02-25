f577255fda559f5f86b30d132778f38df9a1d294
이제헌 <jeheon0717@naver.com>
2023-02-25 21:15:44 +0900

    BOJ 24479 -c++

--- C++/BOJ/24479.cpp
+++ C++/BOJ/24479.cpp
@@ -6,10 +6,9 @@
 using namespace std;
 
 int n, m, r;
-vector<int> visited(0, n + 1);
-vector<vector<int>> graph(m + 1, vector<int>({}));
 
-void dfs()
+
+void dfs(vector<int> visited, vector<vector<int>> graph)
 {
     stack<int> stack;
     int tmp;
@@ -27,6 +26,8 @@ void dfs()
                 stack.push(num);
         }
     }
+    for (int i = 1; i < n + 1; i++)
+        cout<<visited[i]<<"\n";
 }
 
 int main()
@@ -34,18 +35,19 @@ int main()
     cin.tie(0);
 	cout.tie(0);
 	ios::sync_with_stdio(0);
+    cin>>n>>m>>r;
+    vector<int> visited(n + 1, 0);
+    vector<vector<int>> graph(n + 1, vector<int>({}));
 
     int left, right;
-    cin>>n>>m>>r;
     for (int i = 0; i < m; i++)
     {
         cin>>left>>right;
         graph[left].push_back(right);
         graph[right].push_back(left);
     }
-    for (int i = 1; i < m + 1; i++)
+    for (int i = 1; i < n + 1; i++)
         sort(graph[i].begin(), graph[i].end(), greater<int>());
-    dfs();
-    for (int i = 1; i < m + 1; i++)
-        cout<<visited[i]<<"\n";
+    dfs(visited, graph);
+    
 }
